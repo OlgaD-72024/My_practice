af0dfa639caf6ea0636cc40d9dca2ee5aa80a1c9
OlgaD-72024 <yachhik@gmail.com>
2024-07-08 19:56:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+name = "Python"
+tasks = 12
+time = 1.5
+time_average = time / tasks
+print("Курс: " + name, "Всего задач:", tasks, "Затрачено часов:", time, "Среднее время выполнения:", time_average)
